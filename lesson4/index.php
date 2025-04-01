911e71b6ed15851379d41f1b341c85821c231ac7
Arianit173 <arianitvllasaliu@gmail.com>
2025-04-01 16:07:40 +0200

    ndryshime

--- lesson4/index.php
+++ lesson4/index.php
@@ -58,7 +58,7 @@
 // }
 ?>
 
-<?php 
+ <?php 
 // function divisible(){
     
 //     if(($n % 2) == 0){
@@ -144,9 +144,50 @@
 
 ?>
 <?php 
-$sports= ['Real Madrid', 'Barcelona', 'Arsenal', 'Bayern'];
+// $sports= ['Real Madrid', 'Barcelona', 'Arsenal', 'Bayern'];
 
-array_pop($sports);
-var_dump($sports);
+// array_pop($sports);
+// var_dump($sports);
+
+?>
+
+<?php  
+
+// $sports= array('Real Madrid', 'Barcelona', 'Arsenal', 'Bayern');
+// array_unshift($sports);
+// var_dump();
+
+?>
+
+<?php 
+// $sports= array('Real Madrid', 'Barcelona', 'Arsenal', 'Bayern');
+// array.shift($sports);
+// var_dumps($sports);
+
+
+?>
+<?php 
+
+// $sports=["Real Madrid", "Barcelona", "Arsenal", "Bayern"];
+// $output=array_splice($sports,2);
+// $output1=array_splice($sports,3);
+// $output2=array_splice($sports,-2,1);
+// var_dump($output,$output1,$output2);
+
+
+?>
+
+<?php 
+// $sports=[12,24,36,48,60];
+
+// var_dump(array_sum($sports));
+
+?>
+
+<?php 
+$temp =[25, 30, 25, 26, 20, 28, 25]
+$average_temp=array_sum($temp)/7;
+
+echo $average_temp;
 
 ?>
