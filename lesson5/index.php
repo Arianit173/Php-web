6fabf00dfd1b6db198c7eb18ef01fe20ef8457ac
Arianit173 <arianitvllasaliu@gmail.com>
2025-04-15 16:04:37 +0200

    ndryshime

--- lesson5/index.php
+++ lesson5/index.php
@@ -1,4 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // $array =array(
+    //     array(1,2,3),
+    //     array(1,2,3),
+    //     array(1,2,3),
+    // );
+    // for($i=0; $i<4;$i++){
+    // for($j=0; $j<4;$j++){
+    //     echo "array $i element $j <br>";
+    // }
+    // }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for($i=0; $i<5;$i++){
+    //     for($j=$i; $j<=6;$j++){
+    //         echo "&nbsp";
+    //     }
+    //     for($j=1; $j<=$i;$j++){
+    //         echo"*";
+            
+    //     }
+    //     echo"<br>";
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Asocciative arrays
+
+    $grade=array(
+        "Math"=>"5",
+        "Art"=>"3",
+        "History"=>"4",
+        "Music"=>"3",
+    )
+
+    // echo"Math grade is ".$grade['Math'];
+
+
+    // $grade =['Math'] ='5',
+    // $grade =['Math'] ='4',
+    // $grade =['Math'] ='3',
+    // $grade =['Math'] ='2',
+
+    // foreach($grade as $subject =>$grade){
+    //     echo" Subject:".$subject ."Grade:".$grade;
+    //     echo"<br>";
+    // }
+?>
+</body>
+</html>
 
 
 
