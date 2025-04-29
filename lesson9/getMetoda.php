c46044c96979c14ad32a4760624c8e8818b2a10c
Arianit173 <arianitvllasaliu@gmail.com>
2025-04-29 16:11:47 +0200

    ndryshime

---
+++ lesson9/getMetoda.php
@@ -0,0 +1,32 @@
+<?php
+   $username = $_GET ['username'];
+   $password = $_GET ['passowrd'];
+
+
+echo $username;
+echo "<br>";
+echo $password;
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="getMetoda.php" method="get"></form>
+
+    <label for="username" >Username</label>
+    <input type="text" id="username" name="username">
+    <label for="password">Password</label>
+    <input type="password" id="password" name="password">
+
+   <input type="submit" value="submit">
+</body>
+</html>
